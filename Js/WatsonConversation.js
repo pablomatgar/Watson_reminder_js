ef2baefe22c7e2d89cfc741783f24e1847a792c5
riglesiaspe.inf <riglesiaspe.inf@upsa.local>
2017-11-30 12:46:25 +0100

    watson

---
+++ Js/WatsonConversation.js
@@ -0,0 +1,12 @@
+Conversation service = new Conversation(Conversation.VERSION_DATE_2017_05_26);
+service.setUsernameAndPassword("{username}",
+                               "{password}");
+
+String workspaceId = "9978a49e-ea89-4493-b33d-82298d3db20d";
+
+InputData input = new InputData.Builder("Hello").build();
+MessageOptions options = new MessageOptions.Builder(workspaceId).input(input).build();
+
+MessageResponse response = service.message(options).execute();
+
+System.out.println(response);
